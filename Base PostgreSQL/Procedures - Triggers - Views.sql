5c55dca8d20e0cc6190ab3442c1cbf06c15b3ae3
eduardo-bm10 <ebolivarminguet@gmail.com>
2023-06-06 16:16:53 -0600

    Update Procedures - Triggers - Views.sql

--- Base PostgreSQL/Procedures - Triggers - Views.sql
+++ Base PostgreSQL/Procedures - Triggers - Views.sql
@@ -52,11 +52,20 @@ SELECT * FROM NUTRITIONIST
 -- Description: Muestras las medidas registradas por un específico usuario dentro de un lapso indicado.
 -- Parameters: SSN, StartDate, FinalDate
 -- Author: Eduardo Bolívar Minguet
-CREATE OR REPLACE PROCEDURE customers_advance_report
+CREATE OR REPLACE FUNCTION customers_advance_report
 (
 	ssn INT, 
 	startDate DATE, 
 	finalDate DATE
+)
+RETURNS TABLE (
+	Patient INT,
+	ReDate DATE,
+	Waist INT,
+	Neck INT,
+	Hips INT,
+	MusclePercentage REAL,
+	FatPercentage REAL
 )
 LANGUAGE PLPGSQL AS $$
 DECLARE
@@ -72,23 +81,33 @@ BEGIN
 		END IF;
 	END LOOP;
 	IF p_exists THEN
-		SELECT 
-			PatientID, 
-			Date, 
-			Waist, 
-			Neck, 
-			Hips, 
-			MusclePercentage, 
-			FatPercentage
-		FROM 
-			MEASUREMENT 
-		WHERE PatientID = ssn AND Date >= startDate AND Date <= finalDate;
+		FOR r_row IN (SELECT 
+							PatientID, 
+							Date, 
+							Waist, 
+							Neck, 
+							Hips, 
+							MusclePercentage, 
+							FatPercentage
+						FROM 
+							MEASUREMENT 
+						WHERE PatientID = ssn AND Date >= startDate AND Date <= finalDate)
+		LOOP
+			Patient := r_row.PatientID;
+			ReDate := r_row.Date;
+			Waist := r_row.Waist;
+			Neck := r_row.Neck;
+			Hips := r_row.Hips;
+			MusclePercentage := r_row.MusclePercentage;
+			FatPercentage := r_row.FatPercentage;
+			RETURN NEXT;
+		END LOOP;
 	ELSE
 		RAISE NOTICE 'Patient with SSN % does not exist', ssn;
 	END IF;
 END; $$
 
-CALL calculate_payment(1)
+select * from customers_advance_report(1112, '2023-05-05', '2023-06-06')
 
 -- Store Procedure: CREATE RECIPE.
 -- Description: Verifica si los ingredientes existen para luego insertar a la base de datos
@@ -97,38 +116,40 @@ CALL calculate_payment(1)
 -- Author: Eduardo Bolívar Minguet
 CREATE OR REPLACE PROCEDURE create_recipe
 (
-	description TEXT,
-	ingredients TEXT[][] -- This syntax is [['IngredientName1', 'Portion1'], ['IngredientName2', 'Portion2'], etc...]
+	description VARCHAR,
+	ingredients INT[],
+	portions INT[]
 )
 LANGUAGE PLPGSQL 
 AS $$
 DECLARE
 	r_row RECORD;
-	i TEXT[];
-	counter INT := 0;
+	i INT;
+	counter1 INT := 1;
+	counter2 INT := 1;
 BEGIN
-	FOR r_row IN (SELECT Description FROM Product) 
+	FOR r_row IN (SELECT Barcode FROM Product) 
 	LOOP
-		IF ingredients[counter] = Description THEN
-			counter := counter + 1;
+		IF ingredients[counter1] = r_row.Barcode THEN
+			counter1 := counter1 + 1;
 			CONTINUE;
 		ELSE
-			RAISE NOTICE 'PRODUCT % DOES NOT EXIST', ingredients[counter];
+			RAISE NOTICE 'PRODUCT % DOES NOT EXIST', ingredients[counter1];
 			EXIT;
 		END IF; 
 	END LOOP;
 	INSERT INTO RECIPE VALUES (DEFAULT, description);
 	FOREACH i IN ARRAY ingredients 
 	LOOP
-		INSERT INTO RECIPE_PRODUCT_ASSOCIATION VALUES (DEFAULT, i[1], CAST(i[2] AS INT), DEFAULT);
+		INSERT INTO RECIPE_PRODUCT_ASSOCIATION (RecipeId, ProductBarcode, ProductPortion) SELECT MAX(ID), i, portions[counter2] FROM RECIPE;
+		counter2 := counter2 + 1;
 	END LOOP;
 END; $$
 
 -- View: CaloriesPerMealTimeOnPlan
 CREATE VIEW CaloriesPerMealTimeOnPlan AS
 	SELECT 
-		PLAN.ID AS PlanId, 
-		PLAN.Description AS PlanDescription, 
+		PLAN.ID AS PlanId,
 		MEAL_TIME.Name AS Mealtime, 
 		SUM(9*PRODUCT.Fat+4*PRODUCT.Carbohydrate+4*PRODUCT.Protein) AS TotalCalories
 	FROM 
@@ -147,12 +168,15 @@ CREATE VIEW TotalRecipeCalories AS
 		RECIPE.ID AS RecipeID,
 		RECIPE.Description AS RecipeDescription,
 		PRODUCT.Description AS Product,
+		(9*Fat + 4*Carbohydrate + 4*Protein) AS ProductCalories,
 		ProductPortion,
-		(9*Fat + 4*Carbohydrate + 4*Protein)*ProductPortion as TotalProductCalories
+		(9*Fat + 4*Carbohydrate + 4*Protein)*ProductPortion AS TotalProductCalories,
+		SUM((9*Fat + 4*Carbohydrate + 4*Protein)*ProductPortion) AS TotalRecipeCalories
 	FROM
 		RECIPE JOIN RECIPE_PRODUCT_ASSOCIATION ON RECIPE.ID = RecipeID JOIN
-		PRODUCT ON ProductBarcode = Barcode;
-		
+		PRODUCT ON ProductBarcode = Barcode
+	GROUP BY RECIPE.ID, RECIPE.DESCRIPTION, PRODUCT.DESCRIPTION, ProductPortion, Fat, Carbohydrate, Protein;
+
 -- View: PatientCurrentMeasures
 CREATE VIEW NonAssociatedClients AS
 	SELECT
@@ -162,6 +186,3 @@ CREATE VIEW NonAssociatedClients AS
 	FROM
 		PATIENT LEFT JOIN PATIENT_NUTRITIONIST_ASSOCIATION ON ID = PatientID
 	WHERE PatientID IS NULL;
-
-
-
