536889d7d31139a237b41da4577da8b73d09f64d
status-tech <63075293+sebasqr22@users.noreply.github.com>
2023-06-07 12:42:16 -0600

    Reporte Casi Listo
    Co-Authored-By: Marco Alonso Rodríguez Villegas <67651562+markorovi@users.noreply.github.com>

--- FrontEnd/src/app/reporte/reporte.component.ts
+++ FrontEnd/src/app/reporte/reporte.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import html2canvas from 'html2canvas';
 import { jsPDF } from 'jspdf';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
+declare var html2pdf: any; // Declaración del objeto html2pdf
+
 
 @Component({
   selector: 'app-reporte',
@@ -11,7 +17,21 @@ import { jsPDF } from 'jspdf';
 export class ReporteComponent implements OnInit{
   fecha = "";
   nombre = 'Sebastian Quesada';
-  cedula = '118510858'
+  cedula = '118510858';
+  direccion = '';
+  birthdate = '';
+  email = '';
+  pais = '';
+  max = '';
+
+  //variables para la tabla
+  titulos:string[] = ['TITULO1', 'TITULO2'];
+  filas:string[][] = [];
+
+  //esto es un ejemplo
+  fila1:string[] = ['Fila1 V1', 'Fila1 V2'];
+  //filas.push(fila1);
+
 
   ngOnInit(): void {
     const meses = [
@@ -32,17 +52,101 @@ export class ReporteComponent implements OnInit{
     const year = fechaActual.getFullYear();
     const mes = meses[fechaActual.getMonth()];
     const dia = fechaActual.getDate();
-    this.fecha = `${dia} de ${mes} de ${year}`
+    this.fecha = `${dia} de ${mes} de ${year}`;
+    this.getInfoCliente();
+
+    //ejemplooooo esto lo borra de aca (solo la siguiente linea)
+    this.filas.push(this.fila1);
+
+    this.crearTabla(this.titulos, this.filas);
+    
+    //pantalla de carga poner
+    //this.generatePDF();
+    //setear la ruta
+    //pantalla de carga quitar
   }
 
-  async generatePDF() {
-    const pdfContentEl = document.getElementById('reporte');
+  crearTh(heads:string[]){
+    const tr = document.createElement('tr');
+    heads.forEach((head:string)=>{
+      const th = document.createElement('th');
+      th.scope = "col";
+      th.className = "fs-sm text-dark text-uppercase-bold-sm px-0";
+      th.innerText = head;
+      tr.appendChild(th);
+    })
+    return tr;
+  }
 
-    const doc = new jsPDF();
+  crearContenido(contenido:string[][]){
+    const listaTodo:HTMLTableRowElement[] = [];
 
+    const largo = contenido.length;
+    for (let i = 0; i < largo; i++) {
+      const tr = document.createElement('tr');
+      const infoLargo = contenido[i].length;
+      for (let j = 0; j < infoLargo; j++){
+        const td = document.createElement('td');
+        td.className = "px-0";
+        td.innerText = contenido[i][j];
+        tr.appendChild(td);
+      }
+      listaTodo.push(tr);
+    }
+    return listaTodo;
+  }
+
+  crearTabla(heads:string[], contenido:string[][]){
+    const table = document.createElement('table');
+    table.className = "table border-bottom border-gray-200 mt-3";
+
+    const thead = document.createElement('thead');
+    thead.appendChild(this.crearTh(heads));
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+    const cont = this.crearContenido(contenido);
+    cont.forEach((row:HTMLTableRowElement) => {
+      tbody.appendChild(row);
+    })
+    table.appendChild(tbody);
+    
+
+
+    const todo = document.getElementById('tabla') as HTMLDivElement;
+    todo.append(table);
+  }
+
+  getInfoCliente(){
+    let data = localStorage.getItem('usuario');
     // @ts-ignore
-    await doc.html(pdfContentEl.innerHTML).save('test.pdf');
+    data = JSON.parse(data);
+    console.log(data);
+    // @ts-ignore
+    this.nombre = `${data.firstname} ${data.lastname1} ${data.lastname2}`;
+    // @ts-ignore
+    this.direccion = data.address;
+    // @ts-ignore
+    this.birthdate = data.birthdate;
+    // @ts-ignore
+    this.email = data.email;
+    // @ts-ignore
+    this.cedula = data.id;
+     // @ts-ignore
+    this.pais = data.country;
+      // @ts-ignore
+    this.max = data.maxconsumption;
   }
+
+  generatePDF() {
+    const reporte = document.getElementById('reporte') as HTMLDivElement;
+    console.log(reporte);
+    html2pdf().from(reporte).save();
+  }
+
+
+
+
 
 
 }
