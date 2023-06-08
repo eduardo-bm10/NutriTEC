61aad9691994ec44e9cfcfcba3a4daa034bd1233
Jimena León <85376454+menaleon@users.noreply.github.com>
2023-06-08 01:06:28 -0600

    XML Comments for PatientController

--- API_PostgreSQL/Controllers/PatientController.cs
+++ API_PostgreSQL/Controllers/PatientController.cs
@@ -11,12 +11,19 @@ using System.Threading.Tasks;
 
 namespace Postgre_API.Controllers
 {
+    /// <summary>
+    /// Controller for managing patients.
+    /// </summary>
     [Route("api/Patients")]
     [ApiController]
     public class PatientsController : ControllerBase
     {
         private readonly NutritecDbContext _dbContext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PatientsController"/> class.
+        /// </summary>
+        /// <param name="dbContext">The database context.</param>
         public PatientsController(NutritecDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -37,6 +44,10 @@ namespace Postgre_API.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves all patients.
+        /// </summary>
+        /// <returns>A list of patients.</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Patient>>> GetPatients()
         {
@@ -46,8 +57,12 @@ namespace Postgre_API.Controllers
             {
                 return BadRequest(new {message = e.Message});
             }}
-        
-
+    
+        /// <summary>
+        /// Retrieves a specific patient by ID.
+        /// </summary>
+        /// <param name="id">The ID of the patient.</param>
+        /// <returns>The requested patient.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<Patient>> GetPatient(string id)
         {
@@ -66,6 +81,27 @@ namespace Postgre_API.Controllers
             }
         }
 
+        /// <summary>
+        /// Creates a new patient.
+        /// </summary>
+        /// <param name="id">The ID of the patient.</param>
+        /// <param name="firstname">The first name of the patient.</param>
+        /// <param name="lastname1">The first last name of the patient.</param>
+        /// <param name="lastname2">The second last name of the patient.</param>
+        /// <param name="email">The email address of the patient.</param>
+        /// <param name="password">The password of the patient.</param>
+        /// <param name="weight">The weight of the patient.</param>
+        /// <param name="bmi">The BMI (Body Mass Index) of the patient.</param>
+        /// <param name="address">The address of the patient.</param>
+        /// <param name="birthdate">The birthdate of the patient.</param>
+        /// <param name="country">The country of the patient.</param>
+        /// <param name="maxconsumption">The maximum consumption of the patient.</param>
+        /// <param name="waist">The waist measurement of the patient.</param>
+        /// <param name="neck">The neck measurement of the patient.</param>
+        /// <param name="hips">The hips measurement of the patient.</param>
+        /// <param name="musclePercentage">The muscle percentage of the patient.</param>
+        /// <param name="fatPercentage">The fat percentage of the patient.</param>
+        /// <returns>The created patient.</returns>
         [HttpPost("post/{id}/{firstname}/{lastname1}/{lastname2}/{email}/{password}/{weight}/{bmi}/{address}/{birthdate}/{country}/{maxconsumption}/{waist}/{neck}/{hips}/{musclePercentage}/{fatPercentage}")]
         public async Task<IActionResult> CreatePatient(string id, string firstname, string lastname1, string lastname2, string email, string password, int weight, double bmi, string address, DateTime birthdate, string country, double maxconsumption, double waist, double neck, double hips, double musclePercentage, double fatPercentage)
         {
@@ -126,6 +162,22 @@ namespace Postgre_API.Controllers
             }
             }
 
+        /// <summary>
+        /// Updates an existing patient.
+        /// </summary>
+        /// <param name="id">The ID of the patient to update.</param>
+        /// <param name="firstname">The updated first name of the patient.</param>
+        /// <param name="lastname1">The updated first last name of the patient.</param>
+        /// <param name="lastname2">The updated second last name of the patient.</param>
+        /// <param name="email">The updated email address of the patient.</param>
+        /// <param name="password">The updated password of the patient.</param>
+        /// <param name="weight">The updated weight of the patient.</param>
+        /// <param name="bmi">The updated BMI (Body Mass Index) of the patient.</param>
+        /// <param name="address">The updated address of the patient.</param>
+        /// <param name="birthdate">The updated birthdate of the patient.</param>
+        /// <param name="country">The updated country of the patient.</param>
+        /// <param name="maxconsumption">The updated maximum consumption of the patient.</param>
+        /// <returns>The status of the update operation.</returns>
         [HttpPut("put/{id}")]
         public async Task<IActionResult> UpdatePatient(string id, string firstname, string lastname1, string lastname2, string email, string password, int weight, double bmi, string address, DateTime birthdate, string country, double maxconsumption)
         {
@@ -159,6 +211,11 @@ namespace Postgre_API.Controllers
             return BadRequest(new { message =  e.Message});
         }}
 
+        /// <summary>
+        /// Deletes a patient.
+        /// </summary>
+        /// <param name="id">The ID of the patient to delete.</param>
+        /// <returns>The status of the delete operation.</returns>
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeletePatient(string id)
         {
