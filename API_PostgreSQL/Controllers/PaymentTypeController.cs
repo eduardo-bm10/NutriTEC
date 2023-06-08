0eba71606089387bff0c7e0ff06a28de4d04a56b
Jimena León <85376454+menaleon@users.noreply.github.com>
2023-06-08 01:13:50 -0600

    XML Comments for PaymentTypeController

--- API_PostgreSQL/Controllers/PaymentTypeController.cs
+++ API_PostgreSQL/Controllers/PaymentTypeController.cs
@@ -7,17 +7,28 @@ using Postgre_API.Models;
 
 namespace Postgre_API.Controllers
 {
+    /// <summary>
+    /// Controller for managing payment types in the API.
+    /// </summary>
     [Route("api/PaymentType")]
     [ApiController]
     public class PaymentTypeController : ControllerBase
     {
         private readonly NutritecDbContext _dbContext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaymentTypeController"/> class.
+        /// </summary>
+        /// <param name="dbContext">The Nutritec database context.</param>
         public PaymentTypeController(NutritecDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
+        /// <summary>
+        /// Retrieves all payment types.
+        /// </summary>
+        /// <returns>A list of payment types.</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PaymentType>>> GetPaymentTypes()
         {
@@ -28,6 +39,11 @@ namespace Postgre_API.Controllers
                 return BadRequest(new {message = e.Message});
             }}
 
+        /// <summary>
+        /// Retrieves a specific payment type by ID.
+        /// </summary>
+        /// <param name="id">The ID of the payment type to retrieve.</param>
+        /// <returns>The payment type with the specified ID.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<PaymentType>> GetPaymentType(int id)
         {
@@ -45,6 +61,12 @@ namespace Postgre_API.Controllers
                 return BadRequest(new {message = e.Message});
             }}
 
+        /// <summary>
+        /// Creates a new payment type.
+        /// </summary>
+        /// <param name="id">The ID of the payment type.</param>
+        /// <param name="descripcion">The description of the payment type.</param>
+        /// <returns>The created payment type.</returns>
         [HttpPost("post/{descripcion}")]
         public async Task<IActionResult> CreatePaymentType(int id, string descripcion)
         {
@@ -67,6 +89,12 @@ namespace Postgre_API.Controllers
                 return BadRequest(new {message = e.Message});
             }}
 
+        /// <summary>
+        /// Updates an existing payment type.
+        /// </summary>
+        /// <param name="id">The ID of the payment type to update.</param>
+        /// <param name="paymentType">The updated payment type object.</param>
+        /// <returns>A response indicating the success of the update operation.</returns>
         [HttpPut("put/{id}")]
         public async Task<IActionResult> UpdatePaymentType(int id, PaymentType paymentType)
         {
@@ -100,6 +128,11 @@ namespace Postgre_API.Controllers
                 return BadRequest(new {message = e.Message});
             }}
 
+        /// <summary>
+        /// Deletes a payment type.
+        /// </summary>
+        /// <param name="id">The ID of the payment type to delete.</param>
+        /// <returns>A response indicating the success of the delete operation.</returns>
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeletePaymentType(int id)
         {
